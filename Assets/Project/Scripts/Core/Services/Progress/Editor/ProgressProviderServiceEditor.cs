cbd85d7a2df0e62c63b1dc68c68ae2d9cfc8e567
Lyahasik <48154267+Lyahasik@users.noreply.github.com>
2024-04-02 15:23:17 +0300

    Fixed: - editor script - text in inventory cell view

--- Assets/Project/Scripts/Core/Services/Progress/Editor/ProgressProviderServiceEditor.cs
+++ Assets/Project/Scripts/Core/Services/Progress/Editor/ProgressProviderServiceEditor.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace UndergroundFortress.Core.Services.Progress
 {
-    public partial class ProgressProviderService
+    public class ProgressProviderServiceEditor
     {
         [MenuItem("MyLogic/Progress/Reset")]
         public static void Reset()
